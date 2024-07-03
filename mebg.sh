7b4cf5518330a5d3898a7b698128eee8a754bd96
f2e6 <95947898+f2e6@users.noreply.github.com>
2024-07-03 10:29:48 -0400

    Update mebg.sh

--- mebg.sh
+++ mebg.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env sh
 swww img $(find ~/Pictures/wallpapers/ -type f | fzf --preview 'kitty icat --clear --transfer-mode=memory --stdin=no --place="${FZF_PREVIEW_COLUMNS}x${FZF_PREVIEW_LINES}@0x0" {} ')
 
 
